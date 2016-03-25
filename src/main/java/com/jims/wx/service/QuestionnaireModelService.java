6b535e08070e8b9d7c8bd816df5af2b36ec2f25c
zhu <964483087@qq.com>
2016-03-25 14:30:24 +0800

    【朱齐】 实现所有业务逻辑

--- src/main/java/com/jims/wx/service/QuestionnaireModelService.java
+++ src/main/java/com/jims/wx/service/QuestionnaireModelService.java
@@ -3,12 +3,13 @@ package com.jims.wx.service;
 import com.jims.wx.entity.QuestionnaireModel;
 import com.jims.wx.expection.ErrorException;
 import com.jims.wx.facade.QuestionnaireModelFacade;
+import com.jims.wx.vo.BeanChangeVo;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,21 +34,26 @@ public class QuestionnaireModelService {
     }
 
     //条件查询
-    @GET
-    @Path("find-by-id")
-    public QuestionnaireModel findById(@QueryParam("id") String id){
-        return questionnaireModelFacade.getById(id);
-    }
+//    @GET
+//    @Path("find-by-id")
+//    public QuestionnaireModel findById(@QueryParam("id") String id){
+//        return questionnaireModelFacade.getById(id);
+//    }
 
-    //新增
+    /**
+     * 保存增删改
+     *
+     * @param beanChangeVo
+     * @return
+     */
     @POST
-    @Path("add")
-    public Response save (QuestionnaireModel questionnaireModel){
+    @Path("merge")
+    public Response save (BeanChangeVo<QuestionnaireModel> beanChangeVo){
         try {
-            if(null!=questionnaireModel){
-                questionnaireModelFacade.save(questionnaireModel);
-            }
-            return Response.status(Response.Status.OK).entity(questionnaireModel).build();
+            List<QuestionnaireModel> newUpdateModel = new ArrayList<>();
+            newUpdateModel = questionnaireModelFacade.save(beanChangeVo);
+            return Response.status(Response.Status.OK).entity(newUpdateModel).build();
+
         }catch (Exception e) {
             ErrorException errorException = new ErrorException();
             e.printStackTrace();
@@ -61,5 +67,46 @@ public class QuestionnaireModelService {
             }
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
         }
+    }
+
+    @POST
+    @Path("add")
+    public Response save(QuestionnaireModel questionnaireModel) {
+        try {
+            if (null != questionnaireModel) {
+                questionnaireModel = questionnaireModelFacade.save(questionnaireModel);
+            }
+            return Response.status(Response.Status.OK).entity(questionnaireModel).build();
+        } catch (Exception e) {
+            ErrorException errorException = new ErrorException();
+            e.printStackTrace();
+            errorException.setMessage(e);
+            if (errorException.getErrorMessage().toString().indexOf("最大值") != -1) {
+                errorException.setErrorMessage("输入数据超过长度！");
+            } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
+                errorException.setErrorMessage("数据已存在，提交失败！");
+            } else {
+                errorException.setErrorMessage("提交失败！");
+            }
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
+        }
+    }
+
+    @POST
+    @Path("del")
+    public void delQuestionnaireModel(@QueryParam("modelId") String modelIds){
+        System.out.println(modelIds);
+        questionnaireModelFacade.delQuestionnaireModel(modelIds);
+    }
+
+    /**
+     * 根据ID查询问题
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("find-by-id")
+    public QuestionnaireModel findById(@QueryParam("id") String id){
+        return questionnaireModelFacade.getById(id);
     }
 }
