fd96dce2084fd250d591c0ada47481339f8272c2
chenxy <15101128707@163.com>
2016-03-29 16:03:44 +0800

    加载所在医院编号所需的对象

---
+++ src/main/java/com/jims/wx/vo/HospitalInfoDTO.java
@@ -0,0 +1,36 @@
+package com.jims.wx.vo;
+
+/**
+ * Created by chenxiaoyang on 2016/3/28.
+ */
+public class HospitalInfoDTO {
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String  id;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    private String  text;
+
+    public HospitalInfoDTO() {
+
+    }
+    public HospitalInfoDTO(String id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
