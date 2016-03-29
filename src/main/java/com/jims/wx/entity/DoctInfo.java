a76030aef9dd70b5bee40da4cd3ff4436caef5ed
chenxy <15101128707@163.com>
2016-03-29 15:57:29 +0800

    [陈晓阳] 修改了属性的类型。

--- src/main/java/com/jims/wx/entity/DoctInfo.java
+++ src/main/java/com/jims/wx/entity/DoctInfo.java
@@ -1,15 +1,18 @@
 package com.jims.wx.entity;
 
+import java.beans.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.persistence.Transient;
+
+import org.codehaus.jackson.annotate.JsonManagedReference;
 import org.hibernate.annotations.GenericGenerator;
 
 /**
@@ -19,101 +22,134 @@ import org.hibernate.annotations.GenericGenerator;
 @Table(name = "DOCT_INFO", schema = "WX")
 public class DoctInfo implements java.io.Serializable {
 
-	// Fields
 
-	private String id;
-	private String name;
-	private String title;
-	private String hospitalId;
-	private String headUrl;
-	private String description;
-	private Set<ClinicIndex> clinicIndexes = new HashSet<ClinicIndex>(0);
-	// Constructors
-	/** default constructor */
-	public DoctInfo() {
-	}
+    // Fields
+    private String id;
+    private String name;
+    private String title;
+    private String hospitalId;
+    private String headUrl;
+    private byte[] description;//byte[]
+    @Transient
+    private String tranDescription;
 
-	/** minimal constructor */
-	public DoctInfo(String name) {
-		this.name = name;
-	}
+//    private Set<ClinicIndex> clinicIndexes = new HashSet<ClinicIndex>(0);
 
-	/** full constructor */
-	public DoctInfo(String name, String title, String hospitalId,
-			String headUrl, String description, Set<ClinicIndex> clinicIndexes) {
-		this.name = name;
-		this.title = title;
-		this.hospitalId = hospitalId;
-		this.headUrl = headUrl;
-		this.description = description;
-		this.clinicIndexes = clinicIndexes;
-	}
 
-	// Property accessors
-	@GenericGenerator(name = "generator", strategy = "uuid.hex")
-	@Id
-	@GeneratedValue(generator = "generator")
-	@Column(name = "ID", unique = true, nullable = false, length = 64)
-	public String getId() {
-		return this.id;
-	}
+    // Constructors
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    /**
+     * default constructor
+     */
+    public DoctInfo() {
+    }
 
-	@Column(name = "NAME", nullable = false, length = 20)
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * minimal constructor
+     */
+    public DoctInfo(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * full constructor
+     */
+    public DoctInfo(String name, String title, String hospitalId,
+                    String headUrl, byte[] description) {
+        this.name = name;
+        this.title = title;
+        this.hospitalId = hospitalId;
+        this.headUrl = headUrl;
+        this.description = description;
+//		this.clinicIndexes = clinicIndexes;
+    }
 
-	@Column(name = "TITLE", length = 10)
-	public String getTitle() {
-		return this.title;
-	}
+    // Property accessors
+    @GenericGenerator(name = "generator", strategy = "uuid.hex")
+    @Id
+    @GeneratedValue(generator = "generator")
+    @Column(name = "ID", unique = true, nullable = false, length = 64)
+    public String getId() {
+        return this.id;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	@Column(name = "HOSPITAL_ID", length = 64)
-	public String getHospitalId() {
-		return this.hospitalId;
-	}
+    @Column(name = "NAME", nullable = false, length = 20)
+    public String getName() {
+        return this.name;
+    }
 
-	public void setHospitalId(String hospitalId) {
-		this.hospitalId = hospitalId;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Column(name = "HEAD_URL", length = 1024)
-	public String getHeadUrl() {
-		return this.headUrl;
-	}
+    @Column(name = "TITLE", length = 10)
+    public String getTitle() {
+        return this.title;
+    }
 
-	public void setHeadUrl(String headUrl) {
-		this.headUrl = headUrl;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	@Column(name = "DESCRIPTION")
-	public String getDescription() {
-		return this.description;
-	}
+    @Column(name = "HOSPITAL_ID", length = 64)
+    public String getHospitalId() {
+        return this.hospitalId;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "doctInfo")
-	public Set<ClinicIndex> getClinicIndexes() {
-		return this.clinicIndexes;
-	}
+    @Column(name = "HEAD_URL", length = 1024)
+    public String getHeadUrl() {
+        return this.headUrl;
+    }
 
-	public void setClinicIndexes(Set<ClinicIndex> clinicIndexes) {
-		this.clinicIndexes = clinicIndexes;
-	}
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+
+//    @JsonManagedReference
+//	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "doctInfo")
+//	public Set<ClinicIndex> getClinicIndexes() {
+//		return this.clinicIndexes;
+//	}
+//
+//	public void setClinicIndexes(Set<ClinicIndex> clinicIndexes) {
+//		this.clinicIndexes = clinicIndexes;
+//	}
+
+    public void setTranDescription(String tranDescription) {
+        this.tranDescription = tranDescription;
+    }
+
+    @Column(name = "description")
+    public byte[] getDescription() {
+        return description;
+    }
+
+    public void setDescription(byte[] description) {
+        this.description = description;
+    }
+
+    @Transient
+    public String getTranDescription() {
+        if (this.description != null && !"".equals(this.description)) {
+            String s = "";
+            try {
+                s = new String(this.description, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+        return null;
+    }
+
 
 }
