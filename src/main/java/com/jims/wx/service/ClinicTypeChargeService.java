f8895285e7f5bfa83f7829268db024185606c9e1
fyg408224345 <408224345@qq.com>
2016-03-30 09:28:06 +0800

    【冯宇光】 门诊号类收费列表维护的后台service代码 ClinicTypeChargeService.java

--- src/main/java/com/jims/wx/service/ClinicTypeChargeService.java
+++ src/main/java/com/jims/wx/service/ClinicTypeChargeService.java
@@ -2,13 +2,12 @@ package com.jims.wx.service;
 
 import com.google.inject.Inject;
 import com.jims.wx.entity.ClinicTypeCharge;
+import com.jims.wx.entity.ClinicTypeSetting;
 import com.jims.wx.expection.ErrorException;
 import com.jims.wx.facade.ClinicTypeChargeFacade;
+import com.jims.wx.vo.BeanChangeVo;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +25,51 @@ public class ClinicTypeChargeService {
         this.clinicTypeChargeFacade = clinicTypeChargeFacade;
     }
 
-
+    /**
+     * 查询所有号类
+     * @return
+     */
     @GET
-    @Path("list-all")
-    public List<ClinicTypeCharge> listAll() {
-        return clinicTypeChargeFacade.findAll(ClinicTypeCharge.class);
+    @Path("findAllClinicType")
+    public List<ClinicTypeSetting> findAllClinicType(){
+        return clinicTypeChargeFacade.findAll(ClinicTypeSetting.class);
+    }
+
+    /**
+     * 根据号类ID查询该号类下属所有收费列表
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("findById")
+    public List<ClinicTypeCharge> findById(@QueryParam("id")String id){
+        return clinicTypeChargeFacade.findById(id);
+    }
+
+    /**
+     * 保存增删改
+     * @param beanChangeVo
+     * @return
+     */
+    @POST
+    @Path("merge")
+    public Response save(BeanChangeVo<ClinicTypeCharge> beanChangeVo){
+        try {
+            List<ClinicTypeCharge> clinicTypeCharges = new ArrayList<>();
+            clinicTypeCharges = clinicTypeChargeFacade.save(beanChangeVo);
+            return Response.status(Response.Status.OK).entity(clinicTypeCharges).build();
+        } catch (Exception e) {
+            ErrorException errorException = new ErrorException();
+            errorException.setMessage(e);
+            if (errorException.getErrorMessage().toString().indexOf("最大值") != -1) {
+                errorException.setErrorMessage("输入数据超过长度！");
+            } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
+                errorException.setErrorMessage("数据已存在，保存失败！");
+            } else {
+                errorException.setErrorMessage("保存失败！");
+            }
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
+        }
     }
 
     @POST
