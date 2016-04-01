8a518e72004145f5d6d99e983c6d54cc57b3d4b3
zhu <964483087@qq.com>
2016-04-01 16:43:21 +0800

    【朱齐】 修复调查问卷不能修改BUG

--- src/main/java/com/jims/wx/facade/QuestionnaireModelFacade.java
+++ src/main/java/com/jims/wx/facade/QuestionnaireModelFacade.java
@@ -5,7 +5,6 @@ import com.jims.wx.BaseFacade;
 import com.jims.wx.entity.QuestionnaireModel;
 import com.jims.wx.entity.QuestionnaireVsSubject;
 import com.jims.wx.entity.Subject;
-import com.jims.wx.vo.BeanChangeVo;
 import com.jims.wx.vo.QuestionnaireVsSubjectVo;
 
 import javax.inject.Inject;
@@ -21,76 +20,6 @@ public class QuestionnaireModelFacade extends BaseFacade {
     @Inject
     public QuestionnaireModelFacade (EntityManager entityManager){
         this.entityManager=entityManager;
-    }
-
-
-    //新增
-//    @Transactional
-//    public void save (QuestionnaireModel questionnaireModel){
-//        String ids = questionnaireModel.getSubIds();
-//        questionnaireModel = merge(questionnaireModel);
-//        if(ids!=null && ids.length()>0){
-//            String[] idsArray = ids.split(";");
-//            System.out.print(idsArray.length);
-//            Set<QuestionnaireVsSubject> modelVsSub = new HashSet<QuestionnaireVsSubject>();
-//            if(idsArray.length > 0){
-//                QuestionnaireVsSubject questionnaireVsSubject;
-//                for(String id:idsArray){
-//                    System.out.print(id);
-//                    questionnaireVsSubject = new QuestionnaireVsSubject();
-//
-//                    QuestionnaireModel model = new QuestionnaireModel();
-//                    model.setId(questionnaireModel.getId());
-//                    questionnaireVsSubject.setQuestionnaireModel(model);
-//
-//                    Subject sub = new Subject();
-//                    sub.setId(id);
-//                    questionnaireVsSubject.setSubject(sub);
-//
-//                    modelVsSub.add(questionnaireVsSubject);
-//
-//                }
-//                questionnaireModel.setQuestionnaireVsSubjects(modelVsSub);
-//            }
-//        }
-//
-//        merge(questionnaireModel);
-//    }
-
-    /**
-     * 保存删改
-     *
-     * @param beanChangeVo
-     */
-    @Transactional
-    public List<QuestionnaireModel> save(BeanChangeVo<QuestionnaireModel> beanChangeVo){
-        List<QuestionnaireModel> newUpdateModel = new ArrayList<>();
-//        List<QuestionnaireModel> inserted = beanChangeVo.getInserted();
-        List<QuestionnaireModel> updated = beanChangeVo.getUpdated();
-        List<QuestionnaireModel> deleted = beanChangeVo.getDeleted();
-        List<String> ids = new ArrayList<>();
-
-
-        for (QuestionnaireModel model : updated) {
-            QuestionnaireModel merge = merge(model);
-            newUpdateModel.add(merge);
-        }
-
-        for (QuestionnaireModel model : deleted) {
-            ids.add(model.getId());
-            newUpdateModel.add(model);
-        }
-        for(String id:ids){
-            QuestionnaireModel questionnaireModel=get(QuestionnaireModel.class,id);
-            Set<QuestionnaireVsSubject> deleteData =questionnaireModel.getQuestionnaireVsSubjects();
-            List<String>qvsIds=new ArrayList<>();
-            for(QuestionnaireVsSubject qvs:deleteData){
-                qvsIds.add(qvs.getSeriaNo());
-            }
-            super.removeByStringIds(QuestionnaireVsSubject.class,qvsIds);
-        }
-        this.removeByStringIds(QuestionnaireModel.class, ids);
-        return newUpdateModel;
     }
 
     //新增
@@ -111,6 +40,7 @@ public class QuestionnaireModelFacade extends BaseFacade {
         }
         if(null != questionnaireModel && !questionnaireModel.getSubIds().trim().equals("")){
             String subIds = questionnaireModel.getSubIds();
+            System.out.println(subIds);
             String[] idsArray = subIds.split(";");
 
             if(idsArray.length > 0){
@@ -175,12 +105,12 @@ public class QuestionnaireModelFacade extends BaseFacade {
             while(ite.hasNext()){
                 QuestionnaireVsSubject temp = (QuestionnaireVsSubject)ite.next();
                 vo = new QuestionnaireVsSubjectVo();
-                vo.setId(temp.getSeriaNo());
+                vo.setQuestionVsSubId(temp.getSeriaNo());
                 vo.setQuestModelId(obj.getId());
                 vo.setQuestionType(temp.getSubject().getQuestionType());
                 vo.setQuestionContent(temp.getSubject().getQuestionContent());
                 vo.setPreAnswer(temp.getSubject().getPreAnswer());
-                vo.setSubjectId(temp.getSubject().getId());
+                vo.setId(temp.getSubject().getId());
                 voList.add(vo);
             }
             obj.setQuestionnaireVsSubjectVo(voList);
--- src/main/java/com/jims/wx/service/QuestionnaireModelService.java
+++ src/main/java/com/jims/wx/service/QuestionnaireModelService.java
@@ -3,13 +3,11 @@ package com.jims.wx.service;
 import com.jims.wx.entity.QuestionnaireModel;
 import com.jims.wx.expection.ErrorException;
 import com.jims.wx.facade.QuestionnaireModelFacade;
-import com.jims.wx.vo.BeanChangeVo;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,43 +30,7 @@ public class QuestionnaireModelService {
     public List<QuestionnaireModel> listAll(){
         return questionnaireModelFacade.findAll(QuestionnaireModel.class);
     }
-
-    //条件查询
-//    @GET
-//    @Path("find-by-id")
-//    public QuestionnaireModel findById(@QueryParam("id") String id){
-//        return questionnaireModelFacade.getById(id);
-//    }
-
-    /**
-     * 保存增删改
-     *
-     * @param beanChangeVo
-     * @return
-     */
-    @POST
-    @Path("merge")
-    public Response save (BeanChangeVo<QuestionnaireModel> beanChangeVo){
-        try {
-            List<QuestionnaireModel> newUpdateModel = new ArrayList<>();
-            newUpdateModel = questionnaireModelFacade.save(beanChangeVo);
-            return Response.status(Response.Status.OK).entity(newUpdateModel).build();
-
-        }catch (Exception e) {
-            ErrorException errorException = new ErrorException();
-            e.printStackTrace();
-            errorException.setMessage(e);
-            if (errorException.getErrorMessage().toString().indexOf("最大值") != -1) {
-                errorException.setErrorMessage("输入数据超过长度！");
-            } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
-                errorException.setErrorMessage("数据已存在，提交失败！");
-            } else {
-                errorException.setErrorMessage("提交失败！");
-            }
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
-        }
-    }
-
+    
     @POST
     @Path("add")
     public Response save(QuestionnaireModel questionnaireModel) {
