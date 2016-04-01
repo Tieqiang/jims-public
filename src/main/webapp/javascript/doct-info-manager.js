4e0c496371ccf5550c62acfc8ab1666a1bf294df
zhu <964483087@qq.com>
2016-04-01 16:44:50 +0800

    【朱齐】 修改医生列表样式

--- src/main/webapp/javascript/doct-info-manager.js
+++ src/main/webapp/javascript/doct-info-manager.js
@@ -3,20 +3,25 @@
  */
 $(function () {
     var flag;
+
     /**
      * load datagrid data
      */
     var loadDict = function () {
-//        var s_name = $("#s_name").val();
-//        var s_hospitalId = $("#s_hospitalId").val();
-//        var doctInfo={};
-//        doctInfo.name=s_name;
-//        doctInfo.hospitalId=s_hospitalId;
         $.get("/api/doct-info/get-list", function (data) {
             $("#t_user").datagrid('loadData', data);
         });
     }
     loadDict();
+
+    var reset = function(){
+        $("#id").textbox("setValue","");
+        $("#name").textbox("setValue","");
+        $("#title").textbox("setValue","");
+        $("#headUrl").textbox("setValue","");
+        $("#tranContent").val("")
+    }
+
     /**
      * init datagrid
      */
@@ -24,104 +29,64 @@ $(function () {
         idField: 'id',
         title: '医生信息列表',
         footer: '#tb',
-//        url: loadDict(),
         fit: true,
         fitColumns: true,
+        singleSelect : true,
         striped: true,
-        //nowrap: false ,
         loadMsg: '数据正在加载,请耐心的等待...',
-        rownumbers: true,
-//        singleSelect:true ,
         remoteSort: true,
-        sortName: 'name',
-//        sortOrder: 'desc',
-        columns: [
-            [
-                {
-                    field: 'id',
-                    width: 50,
-                    checkbox: true
-//
-                },
-                {
-                    field: 'name',
-                    title: '医生姓名',
-                    width: 100,
-                    sortable: true,
-                    align: 'center'
-                },
-                {
-                    field: 'title',
-                    title: '医生职称',
-                    width: 100,
-                    sortable: true,
-                    align: 'center'
-//                    hidden: true
-                },
-                {
-                    field: 'headUrl',
-                    title: '头像地址',
-                    align: 'center',
-                    sortable: true,
-                    width: 100
-                },
-                {
-                    field: 'hospitalId',
-                    title: '所在医院编号',
-                    align: 'center',
-                    sortable: true,
-                    width: 100
-                },
-                {
-                    field: 'tranDescription',
-                    title: '个人描述',
-                    align: 'center',
-                    sortable: true,
-                    width: 150
-                }
-            ]
-        ],
-        pagination: true,
-        pageSize: 5,
-        pageList: [5, 15, 30, 50]
+        columns: [[{
+            field: 'id',
+            title: '编号',
+            hidden:'true'
+        }, {
+            field: 'name',
+            title: '医生姓名',
+            width: "20%"
+
+        }, {
+            field: 'title',
+            title: '所属科室',
+            width: "20%"
+
+        }, {
+            field: 'headUrl',
+            title: '头像地址',
+            width: "20%"
+        }, {
+            field: 'hospitalId',
+            title: '所在医院编号',
+            width: "20%"
+        }, {
+            field: 'tranDescription',
+            title: '个人描述',
+            width: "20%"
+        }]]
     });
     /**
      * button of add click
      */
     $("#addBtn").on('click', function () {
+        reset();
         flag = "add";
         $("#fm").get(0).reset();
         $('#dlg').dialog('open').dialog('center').dialog('setTitle', '添加医生信息');
-        loadHospital();
+
     });
-    /**
-     * load hospitalId for combobox
-     */
-    var loadHospital = function loadData() {
-        $('#cc').combobox({
-//            ../../../api/hospital-info/find-hospitalId
-            url: "../../../api/hospital-info/find-hospitalId",//loadDataByUrl(),//;
-            valueField: 'id',
-            textField: 'text'
-        });
 
-    }
-    var loadDataByUrl = function () {
-//        $.postJSON("api/hospital-info/find-hospitalId", function (data) {
-//        }, function (data, status) {
-//        })
-        $.get('api/hospital-info/find-hospitalId', function (r) {
-
-        });
-    }
     /**
      * init combobox
      */
-    $('#cc').combobox({
-        width: 150,
+    $("#cc").combobox({
+        width: 160,
         listWidth: 150,
         listHeight: 100,
-        editable: false
+        url:'/api/hospital-dict/list',
+        method:"GET",
+        mode: 'remote',
+        valueField:'id',
+        textField:'hospitalName'
+        //editable: false
     });
     /**
      * getValue from comboboox
@@ -150,7 +115,6 @@ $(function () {
             doctInfo.hospitalId = getValue();
             doctInfo.headUrl = $("#headUrl").val();
             var description = $("#description").val();
-//            var url = "/api/doct-info/save?id=" + id + "&name=" + name + "&title=" + title + "&hospitalId=" + hospitalId + "&headUrl=" + headUrl + "&description=" + description;
         }
         $.postJSON("/api/doct-info/save?description=" + description, doctInfo, function (data) {
             $('#dlg').dialog('close');
@@ -159,14 +123,6 @@ $(function () {
             $("#fm").get(0).reset();
         }, function (data, status) {
         })
-//        $.get(url, function (data) {
-//
-//            $('#dlg').dialog('close');
-//
-//            $.messager.alert("操作成功！");
-//            loadDict();
-//            $("#fm").get(0).reset();
-//        });
     });
     /**
      * button of edit click
@@ -174,13 +130,8 @@ $(function () {
     $("#editBtn").on('click', function () {
         flag = "edit";
         var arr = $('#t_user').datagrid('getSelections');
-
         if (arr.length != 1) {
-//            $.messager.show({
-//                title: "提示信息",
-//                msg: "只能选择一条记录进行修改！"
-//            });
-            $.messager.alert("系统警告", "只能选择一条记录进行修改！","error");
+            $.messager.alert("系统警告", "请选择一条记录进行修改！","error");
         }
         else {
             $('#dlg').dialog({
@@ -190,11 +141,12 @@ $(function () {
             $("#fm").get(0).reset();
 
             loadSelectedRowData(arr);
-            loadHospital();
+
 
             setValue(arr[0].hospitalId)
         }
     });
+
     /**
      * load selectedRowData
      */
@@ -208,35 +160,25 @@ $(function () {
             description: arr[0].tranDescription
         });
     }
+
     /**
      * button of delete click
      */
     $("#delBtn").on('click', function () {
         var arr = $("#t_user").datagrid("getSelections");
-        if (0 == arr.length)
-//            $.messager.show({
-//                title: "提示信息！",
-//                msg: "!"
-//            });
-            $.messager.alert("系统警告", "必须选择一条记录进行删除！","error");
-        else
+        if (0 == arr.length){
+            $.messager.alert("系统警告", "请选择一条记录进行删除！","error");
+        } else {
             $.messager.confirm("系统提示", "确认要删除吗?", function (r) {
                 if (r) {
-//                    alert(arr.length);
                     var ids = '';
                     for (var i = 0; i < arr.length; i++) {
                         ids += arr[i].id + ",";
                     }
                     ids = ids.substring(0, ids.length - 1);
-//                    alert(ids);
                     $.post('/api/doct-info/delete?ids=' + ids, function (result) {
-//                        $('#t_user').datagrid('reload');
                         loadDict();
                         $('#t_user').datagrid('unselectAll');
-//                        $.messager.show({
-//                            title: '提示信息!',
-//                            msg: '操作成功!'
-//                        });
                         $.messager.alert("系统提示", "操作成功");
                     })
                 }
@@ -244,36 +186,31 @@ $(function () {
                     return;
                 }
             });
+        }
     });
+
     /**
-     * button of search click
+     * 查询
      */
-    $("#searchBtn").on('click', function () {
-        $('#lay').layout('expand', 'north');
-    });
+    //$("#searchBtn").on('click', function () {
+    //    $('#lay').layout('expand', 'north');
+    //});
     /**
      * button of searchSubmitbtn
      */
-    $("#searchSubmitbtn").on('click', function () {
-        var s_name = $("#s_name").val().trim();
-        var s_hospitalId = $("#s_hospitalId").val().trim();
-        if ("" == s_name && "" == s_hospitalId) {
-            loadDict();
-        } else {
-            $.get("/api/doct-info/query-by-condition?name="+s_name+"&hospitalId="+s_hospitalId, function (data) {
-                $("#t_user").datagrid('loadData', data);
-            });
-        }
-    })
+    //$("#searchSubmitbtn").on('click', function () {
+    //    var s_name = $("#s_name").val().trim();
+    //    var s_hospitalId = $("#s_hospitalId").val().trim();
+    //    if ("" == s_name && "" == s_hospitalId) {
+    //        loadDict();
+    //    } else {
+    //        $.get("/api/doct-info/query-by-condition?name="+s_name+"&hospitalId="+s_hospitalId, function (data) {
+    //            $("#t_user").datagrid('loadData', data);
+    //        });
+    //    }
+    //})
     /**
      * button ofclearbtn
      */
-    $("#clearbtn").on("click", function () {
-        $("#mysearch").get(0).reset();
-    });
+
 });
-
-
-
-
-
