0ad672704561901a443def769e583ab690fe99f9
chenxy <15101128707@163.com>
2016-04-06 15:09:11 +0800

    [陈晓阳]@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) 加入了这个注解

--- src/main/java/com/jims/wx/entity/ClinicTypeSetting.java
+++ src/main/java/com/jims/wx/entity/ClinicTypeSetting.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.hibernate.annotations.GenericGenerator;
 
 /**
@@ -18,90 +20,67 @@ import org.hibernate.annotations.GenericGenerator;
  */
 @Entity
 @Table(name = "CLINIC_TYPE_SETTING", schema = "WX", uniqueConstraints = @UniqueConstraint(columnNames = "CLINIC_TYPE"))
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class ClinicTypeSetting implements java.io.Serializable {
+    // Fields
+    private String id;
+    private String clinicType;
+    private String hospitalId;
+    private String appId;
 
-	// Fields
+    /**
+     * default constructor
+     */
+    public ClinicTypeSetting() {
+    }
 
-	private String id;
-	private String clinicType;
-	private String hospitalId;
-	private String appId;
-	//private Set<ClinicTypeCharge> clinicTypeCharges = new HashSet<ClinicTypeCharge>(
-	//		0);
-	//private Set<ClinicIndex> clinicIndexes = new HashSet<ClinicIndex>(0);
-
-	// Constructors
-
-	/** default constructor */
-	public ClinicTypeSetting() {
-	}
-
-
-
-    /** full constructor */
-    public ClinicTypeSetting(String id, String clinicType, String hospitalId, String appId) {
-        this.id = id;
+    /**
+     * full constructor
+     */
+    public ClinicTypeSetting(String clinicType, String hospitalId,
+                             String appId) {
         this.clinicType = clinicType;
         this.hospitalId = hospitalId;
         this.appId = appId;
     }
 
-	// Property accessors
-	@GenericGenerator(name = "generator", strategy = "uuid.hex")
-	@Id
-	@GeneratedValue(generator = "generator")
-	@Column(name = "ID", unique = true, nullable = false, length = 64)
-	public String getId() {
-		return this.id;
-	}
+    // Property accessors
+    @GenericGenerator(name = "generator", strategy = "uuid.hex")
+    @Id
+    @GeneratedValue(generator = "generator")
+    @Column(name = "ID", unique = true, nullable = false, length = 64)
+    public String getId() {
+        return this.id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	@Column(name = "CLINIC_TYPE", unique = true, length = 8)
-	public String getClinicType() {
-		return this.clinicType;
-	}
+    @Column(name = "CLINIC_TYPE", unique = true, length = 8)
+    public String getClinicType() {
+        return this.clinicType;
+    }
 
-	public void setClinicType(String clinicType) {
-		this.clinicType = clinicType;
-	}
+    public void setClinicType(String clinicType) {
+        this.clinicType = clinicType;
+    }
 
-	@Column(name = "HOSPITAL_ID", length = 64)
-	public String getHospitalId() {
-		return this.hospitalId;
-	}
+    @Column(name = "HOSPITAL_ID", length = 64)
+    public String getHospitalId() {
+        return this.hospitalId;
+    }
 
-	public void setHospitalId(String hospitalId) {
-		this.hospitalId = hospitalId;
-	}
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
 
-	@Column(name = "APP_ID", length = 64)
-	public String getAppId() {
-		return this.appId;
-	}
+    @Column(name = "APP_ID", length = 64)
+    public String getAppId() {
+        return this.appId;
+    }
 
-	public void setAppId(String appId) {
-		this.appId = appId;
-	}
-
-	//@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "clinicTypeSetting")
-	//public Set<ClinicTypeCharge> getClinicTypeCharges() {
-	//	return this.clinicTypeCharges;
-	//}
-    //
-	//public void setClinicTypeCharges(Set<ClinicTypeCharge> clinicTypeCharges) {
-	//	this.clinicTypeCharges = clinicTypeCharges;
-	//}
-    //
-	//@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "clinicTypeSetting")
-	//public Set<ClinicIndex> getClinicIndexes() {
-	//	return this.clinicIndexes;
-	//}
-    //
-	//public void setClinicIndexes(Set<ClinicIndex> clinicIndexes) {
-	//	this.clinicIndexes = clinicIndexes;
-	//}
-
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
 }
