0d92e243807fb0266787c64b06abe7064bdf0b72
chenxy <15101128707@163.com>
2016-04-06 15:12:47 +0800

    [陈晓阳]号表业务处理层

--- src/main/java/com/jims/wx/service/ClinicForRegistService.java
+++ src/main/java/com/jims/wx/service/ClinicForRegistService.java
@@ -2,16 +2,24 @@ package com.jims.wx.service;
 
 import com.google.inject.Inject;
 import com.jims.wx.entity.ClinicForRegist;
+import com.jims.wx.entity.ClinicIndex;
+import com.jims.wx.entity.DoctInfo;
+import com.jims.wx.entity.Subject;
 import com.jims.wx.expection.ErrorException;
 import com.jims.wx.facade.ClinicForRegistFacade;
+import com.jims.wx.facade.ClinicIndexFacade;
+import com.jims.wx.facade.ClinicTypeChargeFacade;
+import com.jims.wx.facade.ClinicTypeSettingFacade;
+import com.jims.wx.vo.BeanChangeVo;
+import com.jims.wx.vo.ClinicForRegistVO;
+import com.jims.wx.vo.ComboboxVo;
+import freemarker.template.SimpleDate;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by wangjing on 2016/3/21.
@@ -19,29 +27,152 @@ import java.util.List;
 @Path("clinic-for-regist")
 @Produces("application/json")
 public class ClinicForRegistService {
+
+    private ClinicTypeChargeFacade clinicTypeChargeFacade;
+
     private ClinicForRegistFacade clinicForRegistFacade;
 
-    @Inject
-    public ClinicForRegistService(ClinicForRegistFacade clinicIndexFacade) {
-        this.clinicForRegistFacade = clinicForRegistFacade;
+    private ClinicIndexFacade clinicIndexFacade;
+
+    private ClinicTypeSettingFacade clinicTypeSettingFacade;
+
+
+//    find-clinic-setting-type
+
+    @POST
+    @Path("find-clinic-setting-type")
+    public List<ComboboxVo> findSettingType() {
+        List<ComboboxVo> list = clinicTypeSettingFacade.findComboxData();
+        return list;
     }
 
+    @Inject
+    public ClinicForRegistService(ClinicForRegistFacade clinicForRegistFacade, ClinicIndexFacade clinicIndexFacade, ClinicTypeChargeFacade clinicTypeChargeFacade, ClinicTypeSettingFacade clinicTypeSettingFacade) {
+        this.clinicForRegistFacade = clinicForRegistFacade;
+        this.clinicIndexFacade = clinicIndexFacade;
+        this.clinicTypeChargeFacade = clinicTypeChargeFacade;
+        this.clinicTypeSettingFacade = clinicTypeSettingFacade;
+    }
+
+    @GET
+    @Path("find-info")
+    public ClinicForRegistVO findInfo(@QueryParam("id") String id) {
+        ClinicForRegistVO c = this.clinicForRegistFacade.findInfo(id);
+        return c;
+    }
 
     @GET
     @Path("list-all")
-    public List<ClinicForRegist> listAll() {
-        return clinicForRegistFacade.findAll(ClinicForRegist.class);
+    public List<ClinicForRegist> listAll(@QueryParam("likeName") String clinicIndexName, @QueryParam("likeDate") String date) {
+        List<ClinicForRegist> list = this.clinicForRegistFacade.findAllData(ClinicForRegist.class, clinicIndexName, date);
+        return list;
+    }
+
+    /**
+     * 查询号别信息
+     *
+     * @return
+     */
+    @POST
+    @Path("find-clinic-index-type")
+    public List<ComboboxVo> findClinicIndexType(@QueryParam("typeId") String typeId) {
+        List<ComboboxVo> list = clinicIndexFacade.findClinicIndexType(typeId);
+        return list;
+    }
+
+    @GET
+    @Path("judge-is-regist")
+    public Map<String, Object> judgeIsRegist(@QueryParam("date") String date, @QueryParam("clinicIndexId") String clinicIndexId, @QueryParam("date1") String date1) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            map = clinicForRegistFacade.judgeIsRegist(date, clinicIndexId, date1, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    /**
+     * 生成号表
+     *
+     * @param date
+     * @param clinicIndexId
+     * @return
+     */
+    @GET
+    @Path("regist-table")
+    public Map<String, Object> registTable(@QueryParam("date") String date, @QueryParam("clinicIndexId") String clinicIndexId, @QueryParam("date1") String date1, @QueryParam("desc") String desc, @QueryParam("id") String id) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            date = sdf.format(sdf.parse(date));
+            date1 = sdf.format(sdf.parse(date1));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        map = clinicForRegistFacade.registTable(date, clinicIndexId, date1, desc, id);
+        return map;
     }
 
     @POST
     @Path("save")
-    public Response saveRequestMsg(List<ClinicForRegist> updateList) {
+    public Response saveRequestMsg(@QueryParam("clinicTypeId") String clinicTypeId, @QueryParam("date") String date, ClinicForRegist clinicForRegist) {
         try {
-            List<ClinicForRegist> newUpdateDict = new ArrayList<>();
-            if (updateList != null) {
-                newUpdateDict = clinicForRegistFacade.save(updateList);
+            boolean returnVal = clinicForRegistFacade.isExists(clinicTypeId, date, 1);
+            if (returnVal) {
+                ClinicIndex ci = clinicIndexFacade.findById(clinicTypeId);
+                Date d = new SimpleDateFormat("yyyy-MM-dd").parse(date + "00:00:00");
+                clinicForRegist.setClinicDate(d);
+                clinicForRegist.setClinicIndex(ci);
+                clinicForRegist.setCurrentNo(0);
+                clinicForRegist.setRegistrationNum(0);
+                clinicForRegistFacade.save(clinicForRegist);
+                return Response.status(Response.Status.OK).entity(clinicForRegist).build();
+            } else {
+                return Response.status(Response.Status.FORBIDDEN).entity(clinicForRegist).build();
             }
-            return Response.status(Response.Status.OK).entity(newUpdateDict).build();
+        } catch (Exception e) {
+            ErrorException errorException = new ErrorException();
+            errorException.setMessage(e);
+            if (errorException.getErrorMessage().toString().indexOf("最大值") != -1) {
+                errorException.setErrorMessage("输入数据超过长度！");
+            } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
+                errorException.setErrorMessage("数据已存在，提交失败！");
+            } else {
+                errorException.setErrorMessage("提交失败！");
+            }
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
+        }
+    }
+
+
+    @POST
+    @Path("delete")
+    public Response delete(@QueryParam("ids") String ids) {
+        try {
+            List<String> list = clinicForRegistFacade.delete(ClinicForRegist.class, ids);
+            System.out.println(Response.status(Response.Status.OK).entity(list).build().getStatus());
+            return Response.status(Response.Status.OK).entity(list).build();
+        } catch (Exception e) {
+            ErrorException errorException = new ErrorException();
+            e.printStackTrace();
+            errorException.setMessage(e);
+            System.out.println(Response.status(Response.Status.OK).entity(errorException).build().getStatus());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
+        }
+
+    }
+
+
+    @POST
+    @Path("update")
+    public Response update(@QueryParam("clinicIndexId") String clinicIndexId, @QueryParam("date") String date, ClinicForRegist clinicForRegist) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            clinicForRegist.setClinicDate(sdf.parse(date));
+            clinicForRegist.setClinicIndex(clinicIndexFacade.findById(clinicIndexId));
+            ClinicForRegist v = clinicForRegistFacade.save(clinicForRegist);
+            return Response.status(Response.Status.OK).entity(v).build();
         } catch (Exception e) {
             ErrorException errorException = new ErrorException();
             errorException.setMessage(e);
