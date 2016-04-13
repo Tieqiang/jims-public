f6aedca04dfed5fcc03d4513cfda15c1753333f7
Ellis Zhao <zhaotieqiang@gmail.com>
2016-04-13 11:26:11 +0800

    [ztq] 添加微信公众号SDK

--- src/main/resources/db/migration/V17__Modify_Clinic_Index.sql
+++
@@ -1,2 +0,0 @@
--- Add/modify columns
-alter table Clinic_Index modify CLINIC_TYPE_ID VARCHAR2(64);
--- src/main/resources/db/migration/V20__MODIFY_CLINIC_SCHEDULE.sql
+++
@@ -1,5 +0,0 @@
--- Add/modify columns
-alter table CLINIC_SCHEDULE add time_of_day varchar2(10);
--- Add comments to the columns
-comment on column CLINIC_SCHEDULE.time_of_day
-  is '时间段';
