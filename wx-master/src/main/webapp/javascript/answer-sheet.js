8cf060016d65a84c0ed4866b7ab5aef4852bccb3
wei <742041253@qq.com>
2016-04-22 11:29:59 +0800

    QuestionnaireVsSubjectVo增加图片字段 questionnaire-survey.html微信端调查问卷页面增加多选时问题为图片代码段 answer-sheet.js 和answer-sheet.html 将tree类型改为treegrid，方便查看用户答题记录和图片展示

--- wx-master/src/main/webapp/javascript/answer-sheet.js
+++ wx-master/src/main/webapp/javascript/answer-sheet.js
@@ -1,16 +1,13 @@
 $(function () {
-
     var editIndex;
     var modelId;
-
     //定义右侧多个操作的增删改数据
     var stopEdit = function () {
         if (editIndex || editIndex == 0) {
             $("#dg").datagrid('endEdit', editIndex);
             editIndex = undefined;
         }
-    }
-
+    };
     //右侧答题记录
     $("#dg").datagrid({
         title: '号别',
@@ -20,19 +17,23 @@ $(function () {
             title: '编号',
             field: 'id',
             width: "30%"
-        },{
+        }, {
+            title: '调查问卷Id',
+            field: 'questionnaireId',
+            hidden: 'true'
+        }, {
             title: '微信号',
             field: 'openId',
             width: "15%"
-        },{
+        }, {
             title: '身份证号',
             field: 'patId',
             width: "25%"
-        },{
+        }, {
             title: '答题时间',
             field: 'createTime',
             width: "30%"
-            }
+        }
         ]],
         onClickRow: function (index, row) {
             stopEdit();
@@ -40,11 +41,10 @@ $(function () {
             editIndex = index;
         }
     });
-
     //左侧问卷列表
     $("#typeList").datagrid({
         title: '问卷列表',
-        width:'250px',
+        width: '250px',
         url: "/api/questionnaire-model/list-all",
         mode: 'remote',
         method: 'GET',
@@ -53,11 +53,11 @@ $(function () {
             {
                 title: 'ID',
                 field: 'id',
-                hidden:true
-            },{
+                hidden: true
+            }, {
                 title: '问卷名称',
                 field: 'title',
-                width:'100%'
+                width: '100%'
             }
         ]],
         onClickRow: function (rowIndex, rowData) {
@@ -65,104 +65,91 @@ $(function () {
             if (editIndex || editIndex == 0) {
                 $("#dg").datagrid("endEdit", editIndex);
             }
-            $.get("/api/answer-sheet/find-by-model-id?modelId="+modelId, function (data) {
+            $.get("/api/answer-sheet/find-by-model-id?modelId=" + modelId, function (data) {
                 $("#dg").datagrid('loadData', data);
-                console.log(data);
             });
         }
     });
-
-
-
-
-
-
-
-    //弹出答题详情
-    $("#dgShow").treegrid({
-        fit: true,
-        idField: "subjectId",
-        treeField: "subjectName",
-        fitColumns: true,
-        title: "问卷",
-        columns: [[{
-            title: 'id',
-            field: 'id',
-            hidden:true
-        },{
-            title: '名称',
-            field: 'preAnswer',
-            width:"60%"
-        },{
-            title: '科室',
-            field: 'subjectId',
-            width:"40%"
-        }]],
-        onClickRow: function (row) {
-
-            if(row.parentFlag!="Y"){
-                clinicIndexId = row.id;   //号类ID赋值
-            }else{
-                clinicIndexId = null;   //号类ID赋值
-            }
-
-            addData();
-
-            $.get("/api/answer-sheet/find-by-model-id", {id: row.id}, function (data) {
-                $("#dg").datagrid('loadData', data);
-            });
-
-        }
-    });
-
-
-    //弹出答题详情
-    //$("#dgOptionShow").datagrid({
-    //    title: '答题列表',
-    //    width:'100%',
-    //    singleSelect: true,
-    //    columns: [[
-    //        {
-    //            title: '题目',
-    //            field: 'subjectName',
-    //            width:'33%'
-    //        },{
-    //            title: '答题人选择',
-    //            field: 'answerContent',
-    //            width:'33%'
-    //        },{
-    //            title: '默认答案',
-    //            field: 'preAnswer',
-    //            width:'33%'
-    //        }
-    //    ]]
-    //});
-
-
     $("#bottom").datagrid({
         footer: '#tb',
         border: false
     });
-
     $('#cc').layout('panel', 'north').panel('resize', {height: 'auto'});
     $('#cc').layout('panel', 'south').panel('resize', {height: 'auto'});
     $("#cc").layout({
         fit: true
     });
-
-    //详情按钮
+    $("#tree").treegrid({
+        idField: "id",
+        treeField: "questionContent",
+        fitColumns: true,
+        title: "问卷题目列表",
+        columns: [[{
+            title: 'id',
+            field: 'id',
+            hidden: true
+        }, {
+            title: '名称',
+            field: 'questionContent',
+            width: "60%"
+        }, {
+            title: '图案',
+            field: 'picture',
+            width: "40%"
+        }]]
+    });
     $("#detailBtn").on('click', function () {
+        var treeDepts = [];
         var row = $("#dg").datagrid('getSelected');
-        if (row){
-            $.get('/api/answer-sheet/find-by-id?id='+row.id, function (node) {
-                console.log(node);
+        if (row) {
+            $.get('/api/answer-sheet/find-by-id?id=' + row.id, function (select) {
+                console.log(select);
+                $.get('/api/questionnaire-model/find-by-id?id=' + row.questionnaireId, function (node) {
+                    $.each(node.questionnaireVsSubjectVo, function (index, item) {
+                        var obj = {};
+                        obj.id = item.id;
+                        obj.questionContent = item.questionContent;
+                        if (null != item.picture) {
+                            obj.picture = "<img src='" + item.picture + "'style='width:50px;' >";
+                        } else {
+                            obj.picture = null;
+                        }
+                        obj.state = "open";
+                        var children = [];
+                        for (var j = 0; j < item.subjectOptionses.length; j++) {
+                            var opt = item.subjectOptionses[j];
+                            var child = {};
+                            child.id = opt.id;
+                            var firstText = "○ ";
+                            for (var x = 0; x < select.length; x++) {
+                                if (select[x].answer == opt.id) {
+                                    firstText = "● ";
+                                }
+                            }
+                            opt.optContent = firstText + opt.optContent;
+                            child.questionContent = opt.optContent;
 
-                $('#dlg-detail').dialog('open').dialog('center').dialog('setTitle', ' 答题详情');
-                console.log(node);
-                $("#dgShow").treegrid('loadData', node);
+
+                            if (null != opt.image) {
+                                child.picture = "<img src='" + opt.image + "'style='width:50px;' >";
+                            } else {
+                                child.picture = null;
+                            }
+                            children.push(child);
+                        }
+
+                        obj.children = children;
+                        treeDepts.push(obj);
+                    });
+                    console.log(treeDepts);
+                    $('#dlg-detail').dialog('open').dialog('center').dialog('setTitle', ' 答题详情');
+                    $("#tree").treegrid('loadData', treeDepts);
+                })
             })
-        }else{
+
+        } else {
             $.messager.alert("系统提示", "请选择要查看的行");
         }
-    });
-})
+    })
+
+});
