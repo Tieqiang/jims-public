a1461d69273ddc02b4c4324635955fbb15c76f9b
wei <742041253@qq.com>
2016-04-26 09:34:35 +0800

    朱齐 html页面和js文件

--- wx-master/src/main/webapp/javascript/rcpt-master.js
+++ wx-master/src/main/webapp/javascript/rcpt-master.js
@@ -1,14 +1,15 @@
 var app = angular.module("myApp", []);
-var patId="";
-var name="";
+var patId = "";
+var name = "";
 function GetQueryString(name) {
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return  unescape(r[2]); return null;
+    if (r != null)return unescape(r[2]);
+    return null;
 }
-var openId=GetQueryString("openId");
+var openId = GetQueryString("openId");
 app.controller('tableCtrl', function ($scope, $http) {
-    $http.get("/api/rcpt-master/find-by-app-user?openId="+openId)
+    $http.get("/api/rcpt-master/find-by-app-user?openId=" + openId)
         .success(function (data) {
             $("#name").attr("style", "display:block;");
             $("#clinic").attr("style", "display:none;");
@@ -17,18 +18,18 @@ app.controller('tableCtrl', function ($scope, $http) {
             $scope.nameList = data;
         });
 
-    $scope.clickName = function(id,checkName){
-        $http.get("/api/rcpt-master/find-by-pat-id?patId="+id)
-            .success(function(data){
-                if(data.length>0){
-                    patId=id;
-                    name=checkName;
+    $scope.clickName = function (id, checkName) {
+        $http.get("/api/rcpt-master/find-by-pat-id?patId=" + id)
+            .success(function (data) {
+                if (data.length > 0) {
+                    patId = id;
+                    name = checkName;
                     $("#name").attr("style", "display:none;");
                     $("#clinic").attr("style", "display:block;");
                     $("#rcpt").attr("style", "display:none;");
                     $("#items").attr("style", "display:none;");
-                    $scope.clinicList=data;
-                }else{
+                    $scope.clinicList = data;
+                } else {
                     $("#name").attr("style", "display:none;");
                     $("#clinic").attr("style", "display:none;");
                     $("#rcpt").attr("style", "display:none;");
@@ -39,44 +40,44 @@ app.controller('tableCtrl', function ($scope, $http) {
             })
     }
 
-    $scope.clickPatient = function(){
-        $http.get("/api/rcpt-master/find-by-patient?patientId="+patId)
-            .success(function(data){
+    $scope.clickPatient = function () {
+        $http.get("/api/rcpt-master/find-by-patient?patientId=" + patId)
+            .success(function (data) {
                 $('#checkName').html(name);
                 $("#name").attr("style", "display:none;");
                 $("#clinic").attr("style", "display:none;");
                 $("#rcpt").attr("style", "display:block;");
                 $("#items").attr("style", "display:none;");
-                $scope.outpList=data;
+                $scope.outpList = data;
             })
     }
 
-    $scope.clickRcpt = function(rcptNo){
-        $http.get("/api/rcpt-master/find-by-rcpt?rcptNo="+rcptNo)
-            .success(function(data){
+    $scope.clickRcpt = function (rcptNo) {
+        $http.get("/api/rcpt-master/find-by-rcpt?rcptNo=" + rcptNo)
+            .success(function (data) {
                 $("#name").attr("style", "display:none;");
                 $("#clinic").attr("style", "display:none;");
                 $("#rcpt").attr("style", "display:none;");
                 $("#items").attr("style", "display:block;");
-                $scope.rctpList=data;
+                $scope.rctpList = data;
             })
     }
 
-    $scope.backPatient = function(){
+    $scope.backPatient = function () {
         $("#name").attr("style", "display:none;");
         $("#clinic").attr("style", "display:none;");
         $("#rcpt").attr("style", "display:block;");
         $("#items").attr("style", "display:none;");
     }
 
-    $scope.backNameList = function(){
+    $scope.backNameList = function () {
         $("#name").attr("style", "display:block;");
         $("#clinic").attr("style", "display:none;");
         $("#rcpt").attr("style", "display:none;");
         $("#items").attr("style", "display:none;");
     }
 
-    $('#nullRcptOk').click(function (){
+    $('#nullRcptOk').click(function () {
         $('#nullRcpt').attr("style", "display:none;");
         $("#name").attr("style", "display:block;");
         $("#clinic").attr("style", "display:none;");
