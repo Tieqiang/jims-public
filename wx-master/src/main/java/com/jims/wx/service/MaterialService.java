f38206744e5805a7e4aa939f1e513cdfacc79bb2
fyg408224345 <408224345@qq.com>
2016-05-04 08:17:27 +0800

    [冯宇光] 素材管理：图片上传service MaterialService.java

--- wx-master/src/main/java/com/jims/wx/service/MaterialService.java
+++ wx-master/src/main/java/com/jims/wx/service/MaterialService.java
@@ -5,10 +5,11 @@ import com.sun.jersey.multipart.FormDataParam;
 import weixin.popular.api.MaterialAPI;
 import weixin.popular.api.MediaAPI;
 import weixin.popular.api.MessageAPI;
+import weixin.popular.bean.BaseResult;
 import weixin.popular.bean.material.Description;
 import weixin.popular.bean.material.MaterialBatchgetResult;
+import weixin.popular.bean.material.MaterialBatchgetResultItem;
 import weixin.popular.bean.media.Media;
-import weixin.popular.bean.media.UploadimgResult;
 import weixin.popular.bean.message.Article;
 import weixin.popular.bean.message.MessageSendResult;
 import weixin.popular.bean.message.massmessage.Filter;
@@ -20,7 +21,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
 import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by heren on 2016/4/19.
@@ -28,66 +33,79 @@ import java.util.List;
 @Produces("application/json")
 @Path("material")
 public class MaterialService {
-
+    private static final String BASE_MATERIAL_PATH = "e:\\weixin\\material"; //素材保存到本地的根目录
+    private String token = TokenManager.getDefaultToken();                        //公众号唯一凭证
     /**
-     * 获取媒体类型
-     *
+     * 获取永久素材列表
      * @param type 素材的类型，图片（image）、视频（video）、语音 （voice）、图文（news）
      */
     @GET
     @Path("list")
     public MaterialBatchgetResult getMediaList(@QueryParam("type") String type) throws IOException {
-        MaterialBatchgetResult materialBatchgetResult = MaterialAPI.materialBatchget_material(TokenManager.getDefaultToken(), type, 0, 1000);
-
-        byte[] bytes = MaterialAPI.materialGet_material(TokenManager.getDefaultToken(),"8KeHRodiHmGAgrDEQbupZyiKdWahBi-XoXl32JHWTKI") ;
-        File file = new File("d:\\ztq.jpg") ;
-        FileOutputStream fileOutputStream= new FileOutputStream(file) ;
-        fileOutputStream.write(bytes);
-        fileOutputStream.flush();
-        fileOutputStream.close();
-
+        MaterialBatchgetResult materialBatchgetResult = MaterialAPI.materialBatchget_material(token, type, 0, 1000);
+        List<MaterialBatchgetResultItem> items = materialBatchgetResult.getItem();
+        for (MaterialBatchgetResultItem item : items) {
+            System.out.println("item.name:" + item.getName());
+            System.out.println("item.media_id:" + item.getMedia_id());
+            System.out.println("item.update_time:" + item.getUpdate_time());
+            System.out.println("url:" + item.getUrl());
+        }
         return materialBatchgetResult;
     }
 
+    /**
+     * 新增永久图片素材
+     * @param inputStream
+     * @param fileDetail
+     * @return
+     * @throws java.io.FileNotFoundException
+     */
     @POST
-    @Path("upload")
+    @Path("upload-img")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response uploadFiel(@FormDataParam("file")InputStream inputStream,
-                               @FormDataParam("file")FormDataContentDisposition fileDetail) throws FileNotFoundException {
-        //Media media=MaterialAPI.materialAdd_material(TokenManager.getDefaultToken(), weixin.popular.bean.media.MediaType.image, inputStream, new Description());
-        //Media media = MediaAPI.mediaUpload(TokenManager.getDefaultToken(), weixin.popular.bean.media.MediaType.image,inputStream);
-        UploadimgResult uploadimgResult =MediaAPI.mediaUploadimg(TokenManager.getDefaultToken(),inputStream) ;
-        /*System.out.println("fileDetail-name:" + fileDetail.getName());  //file
+                               @FormDataParam("file")FormDataContentDisposition fileDetail) throws IOException {
+        //上传图片获取mediaId
+        Media media=MaterialAPI.materialAdd_material(token, weixin.popular.bean.media.MediaType.image,inputStream, new Description());
+
+        System.out.println("fileDetail-name:" + fileDetail.getName());  //file
         System.out.println("fileDetail-type:" + fileDetail.getType());  //form-data
         System.out.println("fileDetail-fileName:" + fileDetail.getFileName());  //csh.jpg
         System.out.println("mediaId:" + media.getMedia_id());
         System.out.println("type:" + media.getType());
-        System.out.println("createdAt:" + media.getCreated_at());*/
-        System.out.println("url:" + uploadimgResult.getUrl());
-        return Response.status(Response.Status.OK).entity(uploadimgResult).build();
+        System.out.println("createdAt:" + media.getCreated_at());
+        System.out.println("url:" + media.getUrl());
+
+        //保存到本地
+        byte[] bytes = MaterialAPI.materialGet_material(token, media.getMedia_id());    //获取到上传的图片文件
+        //设置保存到本地的路径
+        Date now = new Date();
+        DateFormat dfp = new SimpleDateFormat("yyyyMMdd");
+        String fullPath = BASE_MATERIAL_PATH + "\\" + dfp.format(now) + "\\image";
+        File directoryPath = new File(fullPath);
+        if (!directoryPath.exists()) {      //如果路径不存在，先创建
+            directoryPath.mkdirs();
+        }
+        //设置保存文件名
+        DateFormat dfn = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String fileName = new String(fullPath + "\\" + dfn.format(now) + ".jpg");
+        File file = new File(fileName);
+        file.createNewFile();
+        //写到文件
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.write(bytes);
+        fileOutputStream.flush();
+        fileOutputStream.close();
+
+        return Response.status(Response.Status.OK).entity(media).build();
     }
 
     @POST
-    @Path("save")
-    public Response saveArticle(List<Article> articles){
-        String token = TokenManager.getDefaultToken() ;
-        for(Article article:articles){
-            String imgUrl = article.getThumb_media_id() ;
-            URI uri = URI.create(imgUrl) ;
-            //Media media = MaterialAPI.materialAdd_material(token, weixin.popular.bean.media.MediaType.image,uri,new Description());
-            Media media = MediaAPI.mediaUpload(token, weixin.popular.bean.media.MediaType.image,uri);
-            article.setThumb_media_id(media.getMedia_id());
-            article.setShow_cover_pic("1");
-        }
-        //Media media = MaterialAPI.materialAdd_news(TokenManager.getDefaultToken(),articles) ;
-        //MessageAPI.messageMassSendall()
-        Media media1 = MessageAPI.mediaUploadnews(token,articles) ;
-        MassMPnewsMessage massMessage = new MassMPnewsMessage(media1.getMedia_id());
-        Filter filter = new Filter(false,"0");
-        massMessage.setFilter(filter);
+    @Path("del")
+    public Response delMaterial(String media_id){
+        BaseResult result = MaterialAPI.materialDel_material(token, media_id);
 
-        MessageSendResult messageSendResult = MessageAPI.messageMassSendall(token,massMessage) ;
-        return Response.status(Response.Status.OK).entity(media1).build();
+        return Response.status(Response.Status.OK).entity(BaseResult.class).build();
     }
 
 }
