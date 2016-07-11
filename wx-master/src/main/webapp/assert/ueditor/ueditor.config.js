45e5364dc00e0c2526425e0009d2c4198e04642d
chenxy <15101128707@163.com>
2016-07-11 16:29:52 +0800

    [cxy]注释掉了几个不常用的功能-ueditor

--- wx-master/src/main/webapp/assert/ueditor/ueditor.config.js
+++ wx-master/src/main/webapp/assert/ueditor/ueditor.config.js
@@ -33,11 +33,11 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
-            'fullscreen', 'source', '|', 'undo', 'redo', '|',
-            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
-            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
-            'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
-            'directionalityltr', 'directionalityrtl', 'indent', '|'
+            'fullscreen',  '|', 'undo', 'redo', '|',
+            'bold', 'italic', 'underline', 'fontborder', 'subscript', 'autotypeset', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'selectall', '|',
+             '|',
+              'fontsize', '|',
+              'indent', '|'
 //            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
 //            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
 //            'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
